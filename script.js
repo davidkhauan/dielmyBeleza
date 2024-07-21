69235a3c946c72d3114d233266f38777ee9c0589
David Khauan <thalsmatsumira@gmail.com>
2024-07-21 11:00:28 -0300

    feedback

--- script.js
+++ script.js
@@ -14,3 +14,54 @@ document.getElementById("switch").addEventListener("click", () => {
         switchIcon.classList.remove('icon-switch');
     }, 300); // Duração da transição em milissegundos
 });
+
+const feedbacks = [
+    {
+        text: "Lorem ipsum, dolor s quo, numquam deserunis dicta totam.",
+        author: 'David Khauan',
+        location: 'Fortaleza-CE'
+    },
+    {
+        text: "Lorem ipsum, dolor s quo, numquam deserunis dicta totam.",
+        author: 'Maria da Silva',
+        location: 'Fortaleza-CE'
+    },
+    {
+        text: "Lorem ipsum, dolor s quo, numquam deserunis dicta totam.",
+        author: 'Marcus Antônio',
+        location: 'Fortaleza-CE'
+    },
+    {
+        text: "Lorem ipsum, dolor s quo, numquam deserunis dicta totam.",
+        author: 'Gustavo Lima',
+        location: 'Fortaleza-CE'
+    }
+]
+
+let feedbackAtual = 0
+
+function showFeedback (index) {
+    const feedbackTexto = document.querySelector ('.feedback-texto')
+    const autorNome = document.querySelector ('.autor-nome')
+    const autorLocal = document.querySelector ('.autor-local')
+
+    feedbackTexto.textContent = feedbacks [index].text
+    autorNome.textContent = feedbacks [index].author
+    autorLocal.textContent = feedbacks [index].location
+}
+
+function showPrevFeedback() {
+    feedbackAtual = 
+        (feedbackAtual - 1 + feedbacks.length) % feedbacks.length
+    showFeedback (feedbackAtual)
+}
+
+function showNextFeedback() {
+    feedbackAtual = 
+        (feedbackAtual + 1) % feedbacks.length
+    showFeedback (feedbackAtual)
+}
+
+document.addEventListener ("DOMContentLoaded", () => {
+    showFeedback (feedbackAtual)
+})
